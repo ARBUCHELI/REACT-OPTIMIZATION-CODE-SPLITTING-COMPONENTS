c0f30b8af3a5a41c5fcf40928f0a93e8421bbc9e
ARBUCHELI <radio_seattle@hotmail.com>
2023-04-01 15:34:23 -0500

    creating app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import './styles.css';
+
+const Details = React.lazy(() => import('./Details'));
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <h1 style={{textAlign: "center", paddingTop: "1em", fontFamily: "monospace", fontWeight: "bold", fontSize: "3em", color: "#bd5734 "}}>React Optimization Code Splitting Components</h1>
+        <div className='e6-container' style={{width: "50%", margin: "auto", paddingTop: "2%"}}>
+          <div className='e6-content-container'>
+            <img src={require('./logo.png')} alt='Restaurant logo' />
+            <h1>Your reservation is confirmed.</h1>
+          </div>
+          <Details />
+        </div>
+      </div>
+      <footer style={{textAlign: "center", paddingTop: "2em", paddingBottom: "2em", fontFamily: "monospace", fontWeight: "bold", fontSize: "2em", color: "#bd5734 "}}>Bucheli Web © 2023</footer>
+    </>
   );
-}
+};
 
-export default App;
+export default App
